d786baf9b7be9127f445dbaa1174b54f20e24654
Nathan James <masterygreentea@gmail.com>
2018-11-28 16:45:39 -0500

    Award points

--- source_files/cactuspot.cpp
+++ source_files/cactuspot.cpp
@@ -9,6 +9,7 @@
 void printInstructions();
 void playGame();
 void displayScore();
+int awardPoints(int);
 
 int main() {
 	int ticket[3][3], revealed[3][3];
@@ -44,6 +45,27 @@ int main() {
 	
 	system("pause");
 	return 0;
+}
+
+/*
+	int awardPoints(int sum)
+
+	Based on the sum of the numbers in the player's 
+	selected line, "sum", assigns the player a score. 
+*/
+int awardPoints(int sum) {
+	int pointTable[] = {10000, 36, 720, 360, 80, 252, 
+	108, 72, 54, 180, 72, 180, 119, 36, 306, 1080, 
+	144, 1800, 3600};
+
+	/*
+	The payout table starts at 6, so its fine to just
+	subtract six to get the value we want
+	*/
+	int award = pointTable[sum - 6];
+
+	return award;
+
 }
 
 void printInstructions() {
